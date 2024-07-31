efce593196e3452d46d1a473a3568d8cefae7e78
Igor <39418662+xeonprog@users.noreply.github.com>
2024-07-31 08:41:04 +0300

    Update database.py

--- database.py
+++ database.py
@@ -8,7 +8,10 @@ class Database:
         self.conn.commit()
 
     def add_user(self, user_id, name, age):
-        self.c.execute("INSERT INTO users (id, name, age) VALUES (?, ?, ?)", (user_id, name, age))
+        try:
+            self.c.execute("INSERT INTO users (id, name, age) VALUES (?, ?, ?)", (user_id, name, age))
+        except sqlite3.IntegrityError:
+            self.c.execute("UPDATE users SET name = ?, age = ? WHERE id = ?", (name, age, user_id))
         self.conn.commit()
 
     def get_users(self):
